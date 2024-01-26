a588bebe7e301ef254b9aaca9acac95459823bd9
Victor Serrano <vsbagile@gmail.com>
2024-01-26 18:42:58 +0100

    First test

---
+++ typescript/src/tree.ts
@@ -0,0 +1,6 @@
+export function treeBuilder(level:number){
+    let resultado=" x\n"
+                 +"xxx\n"
+                 +" |";
+    return resultado;    
+}
--- typescript/tests/unit/kata-example.spec.ts
+++ typescript/tests/unit/kata-example.spec.ts
@@ -1,5 +1,15 @@
-describe('kata-setup', () => {
-    it('example test', () => {
-        expect(true).toBeTruthy();
+import { treeBuilder } from "../../src/tree";
+
+describe('Tree in Typescript', () => {
+  
+    it('should build a tree based on two levels', () => {
+      let resultado=" x\n"
+                   +"xxx\n"
+                   +" |";
+        expect(treeBuilder(2)).toEqual(resultado);
     });
 });
+
+
+
+
