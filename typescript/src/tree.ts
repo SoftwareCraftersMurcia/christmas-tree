5c55f7c3c7d51976f58f96231012056a2d533301
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-01-26 18:13:55 +0000

    ♻ extract tail generator

--- typescript/src/tree.ts
+++ typescript/src/tree.ts
@@ -1,6 +1,26 @@
 export function treeBuilder(level:number){
-    let resultado=" x\n"
-                 +"xxx\n"
-                 +" |";
-    return resultado;    
+    if (level === 2) {
+        return    " x\n"
+                + "xxx\n"
+                + tailGenerator(level);
+    }   
+    if (level === 4) {
+
+        return      "   x\n"
+                   +"  xxx\n"
+                   +" xxxxx\n"
+                   +"xxxxxxx\n"
+                   +tailGenerator(level)
+    }
+    return "  x\n"
+          +" xxx\n"
+          +"xxxxx\n"
+          +tailGenerator(level);     
+}
+
+
+
+const tailGenerator = (level:number): string => {
+    const tail = "|"
+    return " ".repeat(level - 1) + tail
 }
--- typescript/tests/unit/kata-example.spec.ts
+++ typescript/tests/unit/kata-example.spec.ts
@@ -1,12 +1,32 @@
 import { treeBuilder } from "../../src/tree";
 
+// Scenario: On a tree with 1 level
+// Resolve the first line of the tree level 2 splitting by `\n`: -> "  x"
+// Resolve the body of the tree taking into account that we have 1 less space at the beginning: " xxx"
+
 describe('Tree in Typescript', () => {
-  
     it('should build a tree based on two levels', () => {
-      let resultado=" x\n"
+      const tree=" x\n"
                    +"xxx\n"
                    +" |";
-        expect(treeBuilder(2)).toEqual(resultado);
+        expect(treeBuilder(2)).toEqual(tree);
+    });
+
+    it('should build a tree based on three levels', () => {
+      const tree="  x\n"
+                   +" xxx\n"
+                   +"xxxxx\n"
+                   +"  |";
+        expect(treeBuilder(3)).toEqual(tree);
+    });
+
+    it('should build a tree based on four levels', () => {
+      const tree="   x\n"
+                   +"  xxx\n"
+                   +" xxxxx\n"
+                   +"xxxxxxx\n"
+                   +"   |";
+        expect(treeBuilder(4)).toEqual(tree);
     });
 });
 
